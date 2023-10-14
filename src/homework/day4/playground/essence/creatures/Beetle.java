09eb9f2b1ea5c11ebf2ab95ab05ef1ae14e4dcbc
aklutskaya <anna.klutskaya@amasty.com>
2023-10-14 20:57:35 +0300

    I did task 2

--- src/homework/day4/playground/essence/creatures/Beetle.java
+++ src/homework/day4/playground/essence/creatures/Beetle.java
@@ -1,4 +1,17 @@
 package homework.day4.playground.essence.creatures;
 
-public class Beetle {
+public class Beetle extends Insect {
+    public Beetle(int mass, String name) {
+        super(mass, name);
+    }
+
+    public void nest(Carrot home) {
+        if (this.getMass() < home.getMass()) {
+            int divisionPart = home.getMass() / this.getMass();
+            String message = String.format("I am %s and I will nest there with %s my family members!", name, divisionPart);
+            System.out.println(message);
+        } else {
+            System.out.println("This carrot is too small for nesting :(\n");
+        }
+    }
 }
--- src/homework/day4/playground/essence/creatures/Vertebrata.java
+++ src/homework/day4/playground/essence/creatures/Vertebrata.java
@@ -1,4 +1,12 @@
 package homework.day4.playground.essence.creatures;
 
 public abstract class Vertebrata extends Animal {
+    public Vertebrata(int mass, String name) {
+        super(mass, name);
+    }
+
+    public void eat(Insect food) {
+        String message = String.format("I am %s and I am eating %s", name, food.getName());
+        System.out.println(message);
+    }
 }
