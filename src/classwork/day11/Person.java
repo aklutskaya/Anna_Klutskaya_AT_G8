ec5df5fa6f99da7c08b7e77bb86d1402a999fca5
aklutskaya <anna.klutskaya@amasty.com>
2023-11-16 20:50:59 +0300

    classwork

---
+++ src/classwork/day11/Person.java
@@ -0,0 +1,13 @@
+package classwork.day11;
+
+public class Person {
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
